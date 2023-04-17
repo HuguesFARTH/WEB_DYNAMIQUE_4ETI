732831677ecd3637263f2648da8491a2dbd0b884
jo-pouradier <jocharbo43@gmail.com>
2023-04-17 09:51:52 +0000

    login

--- Static/login.php
+++ Static/login.php
@@ -1,25 +1,28 @@
 <!DOCTYPE html>
-<link rel="stylesheet" href="css/login.less"></link>
-
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link type="text/css"  rel="stylesheet" href='css/login.less'>
+</head>
 
 <body class="login-page">
     <div class="login-box">
         <h2 class="heading-login-box">Login</h2>
-        <form action="https://messenger.jo-pouradier.fr/form" method="post">
+        <form action="/login-form" method="post">
             <div class="user-box">
-                <input class="user-box-input" type="text" required name="user">
-                <label class="user-box-label">Username</label>
+                <input class="user-box-input" type="email" required name="email" pattern="[a-zA-Z0-9._%+-]+@cpe\.fr">
+                <label class="user-box-label">Username (email cpe)</label>
             </div>
             <div class="user-box">
                 <input class="user-box-input" type="password" required name="password"> 
                 <label class="user-box-label">Password</label>
             </div>
-            <input class="button" type="submit" value="Submit">
-                <span></span>
-                <span></span>
-                <span></span>
-                <span></span>
-            </a>
+            <input class="button" type="submit" value="Connexion">
         </form>
+        <h3 class="not-register">Pas encore inscrit ?
+        <a href="/register">Inscrit toi ici !</a>
+        </h3>
     </div>
 </body>
