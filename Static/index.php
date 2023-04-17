3341475a5eee5239797e28ae74ec5914a5bc9278
Pierre-louis <pierre-louis.telep@cpe.fr>
2023-04-17 10:30:44 +0200

    test

--- Static/index.php
+++ Static/index.php
@@ -1,20 +1,14 @@
 <!DOCTYPE html>
 <link rel="stylesheet" href="css/index.css"></link>
-<script
-    src="https://code.jquery.com/jquery-3.3.1.js"
-    crossorigin="anonymous">
-</script>
-<script> 
-    $(function(){
-      $("#header").load("./html/header.html"); 
-    });
-    $(function(){
-      $("#footer").load("./html/footer.html"); 
-    });
-</script>
+
+
 
 <body>
-    <div id="header"></div>
+    <div id="header">
+    <?php
+echo readfile("./header.html");
+?>
+    </div>
     <div class="container">
 
         <?php 
@@ -25,5 +19,9 @@
         <a href="test.php">to php test</a>
     </div>
     
-    <div id="footer"></div> 
+    <div id="footer">
+    <?php
+echo readfile("./footer.html");
+?>
+    </div> 
 </body>
