166dfb878ca4991d19533c91a78ac7b3070bdef7
jo-pouradier <jocharbo43@gmail.com>
2023-04-17 09:34:34 +0000

    ok pull

---
+++ Static/login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<link rel="stylesheet" href="css/login.less"></link>
+
+
+<body class="login-page">
+    <div class="login-box">
+        <h2 class="heading-login-box">Login</h2>
+        <form action="https://messenger.jo-pouradier.fr/form" method="post">
+            <div class="user-box">
+                <input class="user-box-input" type="text" required name="user">
+                <label class="user-box-label">Username</label>
+            </div>
+            <div class="user-box">
+                <input class="user-box-input" type="password" required name="password"> 
+                <label class="user-box-label">Password</label>
+            </div>
+            <input class="button" type="submit" value="Submit">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+            </a>
+        </form>
+    </div>
+</body>
