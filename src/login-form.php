089217687453cc32e58a6b0d1e507c165ab005b5
Pierrelouis2 <pierre-louis.telep@cpe.fr>
2023-04-19 18:15:31 +0200

    t'er

--- src/login-form.php
+++ src/login-form.php
@@ -3,7 +3,7 @@ require_once('./script.php');
 
 $sql_get_user = "SELECT * FROM users WHERE email = :email";
 $sql_args = array('email' => $_POST['email']);
-$result = requestSQL($request, $sql_args);
+$result = requestSQL($sql_get_user, $sql_args);
 
 
 if(isset($result[0]['password']) == $_POST['password']){
