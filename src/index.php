6b0df7f9d4f69019908b11175daafed0d2acc1f7
jo-pouradier <joseph.pouradier-duteil@cpe.fr>
2023-04-19 17:22:51 +0200

    test

--- src/index.php
+++ src/index.php
@@ -27,7 +27,12 @@ switch ($_GET['page']) {
                     INNER JOIN symptome ON symptome.ids = symptpatho.ids
                     WHERE keywords.name in ('voix','vomissement')";
         // $smarty->assign('result', requestSQL($request));
-        echo (requestSQL($request));
+        $valren = requestSQL($request);
+        $txt = "";
+        for ($i = 0; $i < count($valren, COUNT_RECURSIVE); $i++) {
+            $txt = $txt . "Name : " .$valren[$i]["name"] . "Pathologie : " . $valren[$i]["pathodesc"] . "Symptôme : " . $valren[$i]["symptdesc"] . "Type : " . $valren[$i]["pathotype"] . "ID : " . $valren[$i]["pathoidp"] . "<br>";
+        }
+        echo ($txt);
         // $smarty->display();
         break;
     default:
