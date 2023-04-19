caa51f32804d8464942c8f5b174483f8fa50c850
Hugues <hugues.farthouat@cpe.fr>
2023-04-19 09:07:24 +0200

    ajout search

---
+++ Static/search.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<link rel="stylesheet" href="css/index.less"></link>
+
+<body>
+    <div id="header">
+    <?php include "./html/header.html" ?>
+    </div>
+    <div class="container">
+        <p>Search init</p>
+    </div>
+    
+    <div id="footer">
+    <?php include "./html/footer.html" ?>
+    </div> 
+</body>
