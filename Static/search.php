72a85cdd466630875eae577416e17f523a6898d8
Hugues <hugues.farthouat@cpe.fr>
2023-04-19 09:35:02 +0200

    re

--- Static/search.php
+++ Static/search.php
@@ -10,20 +10,29 @@
         <input class="" placeholder="Que recherchez-vous ?" value="">
         <div class="hidden_child_div">Catégorie de pathologie
             <div class="">
-                <input id="patho_mer" type="checkbox" value="patho_mer">
-                <Label for='patho_mer'>Pathologies de méridien</Label>
-
-                <input id="patho_vis" type="checkbox" value="patho_vis">
-                <Label for='patho_vis'>Pathologies d’organe/viscère (tsang/fu)</Label>
+                <ol>
+                    <li>
+                        <input id="patho_mer" type="checkbox" value="patho_mer">
+                        <Label for='patho_mer'>Pathologies de méridien</Label>
+                    </li>
+                    <li>
+                        <input id="patho_vis" type="checkbox" value="patho_vis">
+                        <Label for='patho_vis'>Pathologies d’organe/viscère (tsang/fu)</Label>
+                    </li>
+                    <li>    
+                        <input id="patho_mus" type="checkbox" value="patho_mus">
+                        <Label for="patho_mus">Pathologies des tendino–musculaires (jing jin)</Label>
+                    </li>
+                    <li>
+                        <input id="patho_bran" type="checkbox" value="patho_bran">
+                        <Label id="patho_bran">Pathologie des branches (voies luo)</Label>
+                    </li>
+                    <li>
+                        <input id="patho_vaiss" type="checkbox" value="patho_vaiss">
+                        <Label for="patho_vaiss">Pathologies des merveilleux vaisseaux</Label>
+                    </li>
+                </ol>
                 
-                <input id="patho_mus" type="checkbox" value="patho_mus">
-                <Label for="patho_mus">Pathologies des tendino–musculaires (jing jin)</Label>
-
-                <input id="patho_bran" type="checkbox" value="patho_bran">
-                <Label id="patho_bran">Pathologie des branches (voies luo)</Label>
-
-                <input id="patho_vaiss" type="checkbox" value="patho_vaiss">
-                <Label for="patho_vaiss">Pathologies des merveilleux vaisseaux</Label>
             </div>
         </div>
 
