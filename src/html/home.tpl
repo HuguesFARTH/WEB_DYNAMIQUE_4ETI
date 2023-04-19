c073f07c5fa5c3ae192b95a24940f1865afcc370
jo-pouradier <jocharbo43@gmail.com>
2023-04-19 09:50:35 +0000

    home

--- src/html/home.tpl
+++ src/html/home.tpl
@@ -1,19 +1,9 @@
-<!DOCTYPE html>
-<link rel="stylesheet" href="css/index.less"></link>
 
-
-
-<body>
-    <div id="header">
-    <?php include "./html/header.html" ?>
-    </div>
+    {include file="html.footer.tpl"};
     <div class="container">
         <?php  echo "<p>Hello World</p>"; ?>
         <a href="test.php">to php test</a>
-        
     </div>
+    {include file="html.footer.tpl";
     
-    <div id="footer">
-    <?php include "./html/footer.html" ?>
-    </div> 
-</body>
+    
--- src/index.php
+++ src/index.php
@@ -3,14 +3,22 @@
 require_once('../libs/Smarty.class.php');
 $smarty = new Smarty();
 
+//$smarty->display('./html/header.tpl');
+//$smarty->display('./html/footer.tpl');
 
-
-$smarty->display('./html/login.tpl');
-if (isset$_POST['register'] == 'register') {
-    echo $_POST['register'];
+switch ($_GET['page']) {
+    case 'login':
+        $smarty->display('./html/login.tpl');
+        break;
+    case 'register':
+        $smarty->display('./html/register.tpl');
+        break;
+    case 'home':
+        $smarty->display('./html/home.tpl');
+        break;
+    default:
+        $smarty->display('./html/home.tpl'); // a changer plus tard
+        break;
 }
 
-if($_POST['page']=='home'){
-    $smarty->display('./html/home.tpl');
-}
 ?>
