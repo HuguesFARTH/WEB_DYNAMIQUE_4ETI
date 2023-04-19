8b315873169fb6bd455123383488fa18852b313d
Pierrelouis2 <pierre-louis.telep@cpe.fr>
2023-04-19 17:58:25 +0200

    sql

--- src/login-form.php
+++ src/login-form.php
@@ -1,6 +1,11 @@
 <?php 
 
 $sql_get_user = "SELECT * FROM users WHERE email = :email AND password = :password";
+$sql_args = array(
+    email => $_POST['email'],
+    password => $_POST['password']
+);
+
 
 if(isset($_POST['email']) == 'pl@cpe.fr'){
     session_start();
