29aa68f152e588b805cff2ac84e068cf91c386ad
Hugues <hugues.farthouat@cpe.fr>
2023-04-19 10:40:48 +0200

    li to ul

--- Static/search.php
+++ Static/search.php
@@ -13,26 +13,26 @@
                 <button class="dropbtn">Catégorie de pathologie</button>
                 <div class="">
                     <ol>
-                        <li>
+                        <ul>
                             <input id="patho_mer" type="checkbox" value="patho_mer">
                             <Label for='patho_mer'>Pathologies de méridien</Label>
-                        </li>
-                        <li>
+                        </ul>
+                        <ul>
                             <input id="patho_vis" type="checkbox" value="patho_vis">
                             <Label for='patho_vis'>Pathologies d’organe/viscère (tsang/fu)</Label>
-                        </li>
-                        <li>    
+                        </ul>
+                        <ul>    
                             <input id="patho_mus" type="checkbox" value="patho_mus">
                             <Label for="patho_mus">Pathologies des tendino–musculaires (jing jin)</Label>
-                        </li>
-                        <li>
+                        </ul>
+                        <ul>
                             <input id="patho_bran" type="checkbox" value="patho_bran">
                             <Label id="patho_bran">Pathologie des branches (voies luo)</Label>
-                        </li>
-                        <li>
+                        </ul>
+                        <ul>
                             <input id="patho_vaiss" type="checkbox" value="patho_vaiss">
                             <Label for="patho_vaiss">Pathologies des merveilleux vaisseaux</Label>
-                        </li>
+                        </ul>
                     </ol>
                 </div>
             </div>
@@ -40,30 +40,30 @@
                 <button class="dropbtn">Caractéristique.s</button>
                 <div class="">
                     <ol>
-                        <li>
+                        <ul>
                             <input id="car_int" type="checkbox" value="car_int">
                             <Label for='car_int'>interne</Label>
-                        </li>
-                        <li>
+                        </ul>
+                        <ul>
                             <input id="car_ext" type="checkbox" value="car_ext">
                             <Label for='car_ext'>externe</Label>
-                        </li>
-                        <li>    
+                        </ul>
+                        <ul>    
                             <input id="car_plein" type="checkbox" value="car_plein">
                             <Label for="car_plein">plein</Label>
-                        </li>
-                        <li>
+                        </ul>
+                        <ul>
                             <input id="car_vide" type="checkbox" value="car_vide">
                             <Label id="car_vide">vide</Label>
-                        </li>
-                        <li>
+                        </ul>
+                        <ul>
                             <input id="car_chaud" type="checkbox" value="car_chaud">
                             <Label for="car_chaud">chaud</Label>
-                        </li>
-                        <li>
+                        </ul>
+                        <ul>
                             <input id="car_froid" type="checkbox" value="car_froid">
                             <Label for="car_froid">froid</Label>
-                        </li>
+                        </ul>
                     </ol>
                 </div>
             </div>
