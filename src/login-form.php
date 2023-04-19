7d1a463a6c85d60191448bb06a5612cd17684c20
Pierrelouis2 <pierre-louis.telep@cpe.fr>
2023-04-19 18:16:20 +0200

    r

--- src/login-form.php
+++ src/login-form.php
@@ -7,6 +7,7 @@ $result = requestSQL($sql_get_user, $sql_args);
 
 
 if(isset($result[0]['password']) == $_POST['password']){
+    echo 'connected';
     session_start();
     $_SESSION['user'] = $result;
     header('Location: /?page=home');
