c2c29207a6ecb7d98e9da6901b57ef0e5085b4e7
Adrien <adrien.dalbeigue@cpe.fr>
2023-04-19 10:57:12 +0200

    req

--- Static/script.php
+++ Static/script.php
@@ -1,18 +1,5 @@
 <?php
 
-// function request ($user, $passwd, $sql) {
-//     $dbh = new PDO($user, $passwd);
-
-//     $dbh->beginTransaction();
-//     try {
-//         $sth = $dbh->prepare($sql);
-//         $data = $sth->fetchAll();
-//         $dbh->commit();
-//     } catch(PDOException $e) {
-//         $dbh->rollback();
-// }
-// }
-
 function requestSQl($username, $password, $dbname, $request) {
     try {
         $db = new PDO("pgsql:host=localhost;port=5432;dbname=$dbname;user=$username;password=$password");
@@ -21,17 +8,18 @@ function requestSQl($username, $password, $dbname, $request) {
         echo "Connection failed: " . $e->getMessage();
         die();
       }
-      
       // Exécution de la requête
       $stmt = $db->prepare($request);
       $stmt->execute();
-      
+
       // Traitement des résultats
+      $valren ="";
       while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-        echo "ID : " . $row["id"] . " - Nom : " . $row["nom"] . "<br>";
+        $valren = $valren . "ID : " . $row["id"] . " - Nom : " . $row["nom"] . "<br>\n";
       }
-      
       // Fermeture de la connexion
       $db = null;
+
+      return $valren;
     }    
 ?>
