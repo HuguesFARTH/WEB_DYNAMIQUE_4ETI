6ed666b0ece48ca62994b4fbf1fff3924397d996
Ixion <hugues.farthouat@cpe.fr>
2023-04-23 22:04:32 +0200

    re

--- v2/search.php
+++ v2/search.php
@@ -19,9 +19,9 @@ if (strlen($jsonData) > 0) {
 //split string
 $keywords = explode(",",strtolower(preg_replace('/\s+/', ',', trim($data['keywords']))));
 $meridiens = $data['meridiens'];
-var_dump($data)
-var_dump($data['meridiens'])
-var_dump($categories)
+var_dump($data);
+var_dump($data['meridiens']);
+var_dump($categories);
 $categories = $data['categories'];
 $caracteristiques = $data['caracteristiques'];
 $sql_args=[]
