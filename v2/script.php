97c5112c3ce4f4960e80920b279e62af7bcc6fb4
Ixion <hugues.farthouat@cpe.fr>
2023-04-23 17:28:54 +0200

    re

---
+++ v2/script.php
@@ -0,0 +1,39 @@
+<?php
+
+function requestSQl($request, $params=null, $dbname="acudb") {
+    try {
+        $db = new PDO("pgsql:host=localhost;port=5432;dbname=". $dbname .";user=pgtp;password=tp");
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      } catch (PDOException $e) {
+        echo "Connection failed: " . $e->getMessage();
+        die();
+      }
+      // Exécution de la requête
+      $stmt = $db->prepare($request);
+      if ($params==null){
+        $stmt->execute();
+      } else {
+        $stmt->execute($params);
+      }
+      
+
+      // Traitement des résultats
+      $valren = $stmt->fetchAll(PDO::FETCH_ASSOC);
+      $db = null;
+
+      return $valren;
+    }   
+
+
+function current_session() {
+  if (session_status() != 'PHP_SESSION_ACTIVE'){
+    return True;
+  } else {
+    return False;
+  }
+
+}
+
+
+
+?>
---
+++ v2/search.php
@@ -0,0 +1,54 @@
+<?php
+
+require_once('./script.php');
+require_once('../libs/Smarty.class.php');
+$smarty = new Smarty();
+
+/* Reception du JSON */
+$jsonData = file_get_contents("php://input");
+        
+/* Verifie si JSON est vide */
+if (strlen($jsonData) > 0) {
+    /* Decoder JSON */
+    $data = json_decode($jsonData, true);
+    /* Verifie les erreurs et le format final */
+    if (!(json_last_error() == JSON_ERROR_NONE and is_array($data)))
+        die('Données JSON invalides.');
+} else
+    die('Aucune données JSON.');
+     
+//split string
+$keywords = explode(",",strtolower(preg_replace('/\s+/', ',', trim($data['keywords']))));
+//varifie si $keywords est vide
+if (count($keywords) == 1 && $keywords[0] == ""){
+    $request = "SELECT patho.mer, keywords.name as keywordsName, patho.idp as pathoIdp, patho.type as pathoType, patho.desc as pathoDesc, symptome.desc as symptDesc
+                    FROM symptpatho
+                    INNER JOIN patho ON patho.idp = symptpatho.idp
+                    INNER JOIN keysympt ON keysympt.ids = symptpatho.ids
+                    INNER JOIN keywords ON keysympt.idk = keywords.idk
+                    INNER JOIN symptome ON symptome.ids = symptpatho.ids";
+    $sql_args = null;
+}else{
+    $request = "SELECT patho.mer, keywords.name as keywordsName, patho.idp as pathoIdp, patho.type as pathoType, patho.desc as pathoDesc, symptome.desc as symptDesc
+                    FROM symptpatho
+                    INNER JOIN patho ON patho.idp = symptpatho.idp
+                    INNER JOIN keysympt ON keysympt.ids = symptpatho.ids
+                    INNER JOIN keywords ON keysympt.idk = keywords.idk
+                    INNER JOIN symptome ON symptome.ids = symptpatho.ids
+                    WHERE keywords.name in (";
+    $request .= implode(',', array_fill(0, count($keywords), '?'));
+    $request .= ")";
+    $sql_args = $keywords;
+}
+$result = requestSQL($request, $sql_args);
+
+foreach ($result as $key => $value) {
+    // echo "<br>";
+    // echo "Patho id:".$value['pathoidp']."<br>";
+    // echo " Name:".$value['name'] . "<br> " ."Patho Desc:". $value['pathodesc'] . "<br> " . "Sympt Desc:".$value['symptdesc'] . "<br> " ."Patho Type:". $value['pathotype']. "<br>";
+    foreach ($value as $k => $v) {
+        $smarty->assign($k,$v);
+    }
+    $smarty->display('./html/pathologie.tpl');
+}
+?>
