56c65446fa346bb88228e86359cdf01cabcc11ec
Ixion <hugues.farthouat@cpe.fr>
2023-04-23 13:59:07 +0200

    yrd

--- src/search.php
+++ src/search.php
@@ -1,3 +1,14 @@
 <?php
-echo $_POST;
+/* Reception du JSON */
+$jsonData = file_get_contents("php://input");
+        
+/* Verifie si JSON est vide */
+if (strlen($jsonData) > 0) {
+    /* Decoder JSON */
+    $data = json_decode($jsonData, true);
+    /* Verifie les erreurs et le format final */
+    if (!(json_last_error() == JSON_ERROR_NONE and is_array($data)))
+        die('Données JSON invalides.');
+} else
+    die('Aucune données JSON.');
 ?>
