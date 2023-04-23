5992fe8a14b80bc8c860dac8264c30dca7e8b520
Ixion <hugues.farthouat@cpe.fr>
2023-04-23 18:29:32 +0200

    re

--- v2/login-form.php
+++ v2/login-form.php
@@ -1,17 +1,15 @@
 <?php 
 require_once('./script.php');
 
-$sql_get_user = "SELECT password FROM users WHERE email = :email";
+$sql_get_user = "SELECT * FROM users WHERE email = :email";
 $sql_args = array('email' => $_POST['email']);
 $result = requestSQL($sql_get_user, $sql_args);
 
 if(count($result) >= 1 && password_verify($_POST['password'], $result[0]['password'])){
-    echo 'connected';
     session_start();
     $_SESSION['user'] = $result;
     header('Location: /?page=home');
 } else {
     header('Location: /?page=login');
 };
-
 ?>
