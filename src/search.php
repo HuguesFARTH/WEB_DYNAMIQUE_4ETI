7a1fa0874ebd06be6c74e037a7daf1f18837f6ea
Ixion <hugues.farthouat@cpe.fr>
2023-04-23 14:23:10 +0200

    test

--- src/search.php
+++ src/search.php
@@ -20,7 +20,7 @@ echo "\n";
      
 // convertie l'array $data['keywords'] en string pour la requete SQL
 //enlève tous les espaces et split la string en array
-$keywords = "\'"+preg_replace('/\s+/', '\',\'', trim($data['keywords']))+"\'";
+$keywords = "\'".preg_replace('/\s+/', '\',\'', trim($data['keywords']))."\'";
 echo $keywords;
 /*$request = "SELECT keywords.name as name, patho.idp as pathoIdp, patho.type as pathoType, patho.desc as pathoDesc, symptome.desc as symptDesc
                     FROM symptpatho
