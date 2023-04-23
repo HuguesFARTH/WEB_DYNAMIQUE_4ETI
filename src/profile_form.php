8a4fda71c3191e8fe96afb5cb46f82f95d14ac9e
Pierrelouis2 <pierre-louis.telep@cpe.fr>
2023-04-23 14:33:03 +0200

    test

--- src/profile_form.php
+++ src/profile_form.php
@@ -1,5 +1,6 @@
 <?php 
 require_once('./script.php');
+session_start();
 
 $sql_create_user = "UPDATE users set password = :password,name = :name ,last_name = :last_name where email = :email";
 $sql_args = array('email' => $_POST['email'] , 'password' => $_POST['password'], 'name' => $_POST['name'], 'last_name' => $_POST['last_name']);
