d0f9c1f5059cb3cd150998c2fdaecf7008c20a3b
Pierrelouis2 <pierre-louis.telep@cpe.fr>
2023-04-23 13:05:49 +0200

    ok login

--- src/login-form.php
+++ src/login-form.php
@@ -5,14 +5,11 @@ $sql_get_user = "SELECT * FROM users WHERE email = :email";
 $sql_args = array('email' => $_POST['email']);
 $result = requestSQL($sql_get_user, $sql_args);
 
-print_r($result);
-if ($result != Array()){
-    if($result[0]['password'] == $_POST['password']){
-        echo 'connected';
-        session_start();
-        $_SESSION['user'] = $result;
-        header('Location: /?page=home');
-    };    
+if($result[0]['password'] == $_POST['password']){
+    echo 'connected';
+    session_start();
+    $_SESSION['user'] = $result;
+    header('Location: /?page=home');
 } else {
     print('login failed');
     header('Location: /?page=login');
