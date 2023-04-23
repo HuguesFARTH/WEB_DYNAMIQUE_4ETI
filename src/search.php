58e16c70d01e9409df05f36a0041891132997706
Ixion <hugues.farthouat@cpe.fr>
2023-04-23 14:20:18 +0200

    test

--- src/search.php
+++ src/search.php
@@ -17,7 +17,7 @@ if (strlen($jsonData) > 0) {
 
 echo $data['keywords'];
 echo "\n";
-/*
+
 $request = "SELECT keywords.name as name, patho.idp as pathoIdp, patho.type as pathoType, patho.desc as pathoDesc, symptome.desc as symptDesc
                     FROM symptpatho
                     INNER JOIN patho ON patho.idp = symptpatho.idp
@@ -25,7 +25,7 @@ $request = "SELECT keywords.name as name, patho.idp as pathoIdp, patho.type as p
                     INNER JOIN keywords ON keysympt.idk = keywords.idk
                     INNER JOIN symptome ON symptome.ids = symptpatho.ids
                     WHERE keywords.name in :keywords;"
-         */           
+     
 // convertie l'array $data['keywords'] en string pour la requete SQL
 //enlève tous les espaces et split la string en array
 $keywords = preg_replace('/\s+/', ',', trim($data['keywords']));
