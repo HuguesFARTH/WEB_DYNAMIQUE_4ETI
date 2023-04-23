737842bac25529f6d976eb596a62a8a5858791cb
Ixion <hugues.farthouat@cpe.fr>
2023-04-23 18:22:15 +0200

    re

--- v2/login-form.php
+++ v2/login-form.php
@@ -4,10 +4,9 @@ require_once('./script.php');
 $sql_get_user = "SELECT password FROM users WHERE email = :email";
 $sql_args = array('email' => $_POST['email']);
 $result = requestSQL($sql_get_user, $sql_args);
-$hashSecure = password_hash($_POST['password'], PASSWORD_DEFAULT);
 echo $hashSecure;
 var_dump($result);
-if(password_verify($hashSecure,$result['password'])){
+if(password_verify($_POST['password'],$result['password'])){
     echo 'connected';
     session_start();
     $_SESSION['user'] = $result;
