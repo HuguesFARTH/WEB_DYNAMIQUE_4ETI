4024ab8a3215c8332109fc0f3006f6d61fd601f3
Ixion <hugues.farthouat@cpe.fr>
2023-04-23 13:40:32 +0200

    yrdy

--- src/search.php
+++ src/search.php
@@ -1,10 +1,6 @@
 <?php
 //renvoie les données du formulaire de recherche
-if (isset($_POST['search'])) {
-    $search = $_POST['search'];
-    $request = "SELECT * FROM acudb WHERE nom_acu LIKE '$search'";
-    $params = array($search);
-    $result = requestSQL($request, $params);
-    print_r($result);
+if (isset($_POST)) {
+    print_r($_POST);
 }
 ?>
