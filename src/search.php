30cdf29704da7ba19a978b12c11d94f0a97481db
Ixion <hugues.farthouat@cpe.fr>
2023-04-23 14:27:28 +0200

    test

--- src/search.php
+++ src/search.php
@@ -22,6 +22,7 @@ echo "\n";
 //enlève tous les espaces et split la string en array
 $keywords = "(\'".preg_replace('/\s+/', '\',\'', trim($data['keywords']))."\')";
 echo $keywords;
+/*
 $request = "SELECT keywords.name as name, patho.idp as pathoIdp, patho.type as pathoType, patho.desc as pathoDesc, symptome.desc as symptDesc
                     FROM symptpatho
                     INNER JOIN patho ON patho.idp = symptpatho.idp
@@ -30,6 +31,6 @@ $request = "SELECT keywords.name as name, patho.idp as pathoIdp, patho.type as p
                     INNER JOIN symptome ON symptome.ids = symptpatho.ids
                     WHERE keywords.name in :keywords;"
 $sql_args = array('keywords' => $keywords);
-$result = requestSQL($request, $sql_args);
+$result = requestSQL($request, $sql_args);*/
 echo $result;
 ?>
