6ff425ac74c9c23ce80c492e28d4a1ef937e0f30
Ixion <hugues.farthouat@cpe.fr>
2023-04-23 14:23:32 +0200

    test

--- src/search.php
+++ src/search.php
@@ -22,14 +22,14 @@ echo "\n";
 //enlève tous les espaces et split la string en array
 $keywords = "\'".preg_replace('/\s+/', '\',\'', trim($data['keywords']))."\'";
 echo $keywords;
-/*$request = "SELECT keywords.name as name, patho.idp as pathoIdp, patho.type as pathoType, patho.desc as pathoDesc, symptome.desc as symptDesc
+$request = "SELECT keywords.name as name, patho.idp as pathoIdp, patho.type as pathoType, patho.desc as pathoDesc, symptome.desc as symptDesc
                     FROM symptpatho
                     INNER JOIN patho ON patho.idp = symptpatho.idp
                     INNER JOIN keysympt ON keysympt.ids = symptpatho.ids
                     INNER JOIN keywords ON keysympt.idk = keywords.idk
                     INNER JOIN symptome ON symptome.ids = symptpatho.ids
                     WHERE keywords.name in :keywords;"
-$sql_args = array('keywords' => "("+$keywords+"(");
-$result = requestSQL($request, $sql_args);*/
+$sql_args = array('keywords' => "(".$keywords.")");
+$result = requestSQL($request, $sql_args);
 echo $result;
 ?>
