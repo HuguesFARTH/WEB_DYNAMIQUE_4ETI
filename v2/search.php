18bd38229986ae80bc8aab252499f9406f33aa7e
Ixion <hugues.farthouat@cpe.fr>
2023-04-23 22:08:14 +0200

    re

--- v2/search.php
+++ v2/search.php
@@ -52,7 +52,7 @@ if (count($keywords) == 1 && $keywords[0] == ""){
     $sql_args = array_merge($sql_args,$keywords);
 }
     
-$request.="GROUP BY patho.idp, meridien.nom 
+$request.=" GROUP BY patho.idp, meridien.nom 
                 ORDER BY patho.idp;";
 var_dump($request);
 $result = requestSQL($request, $sql_args);
