b84fcecf39fdeaf70458d280cd5d2a5705f31ba1
Pierrelouis2 <pierre-louis.telep@cpe.fr>
2023-04-23 15:28:38 +0200

    hjas

--- src/register-form.php
+++ src/register-form.php
@@ -2,9 +2,7 @@
 require_once('./script.php');
 
 $sql_create_user = "INSERT INTO users VALUES (:email,:password,:name ,:last_name)";
-define('PREFIX_SALT', 'asso'); 
-define('SUFFIX_SALT', 'puncture');
-$hashSecure = md5(PREFIX_SALT.$_POST['password'].SUFFIX_SALT); 
+$hashSecure = password_hash($_POST['password'], PASSWORD_DEFAULT); 
 
 $sql_args = array('email' => $_POST['email'] , 'password' => $hashSecure, 'name' => $_POST['name'], 'last_name' => $_POST['last_name']);
 $result = requestSQL($sql_create_user, $sql_args);
