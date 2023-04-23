271f31baef69e9fd22a9e7c7fb025e9bacae77fa
Ixion <hugues.farthouat@cpe.fr>
2023-04-23 13:48:03 +0200

    test

--- src/search.php
+++ src/search.php
@@ -1,8 +1,13 @@
 <?php
- 
+
 header("Content-Type: application/json");
 echo $_POST['keywords'];
 $data = json_decode($_POST);
  
 echo "Hello $data";
+
+
+foreach ($_POST as $key => $value) {
+    echo "$key => $value";
+}
 ?>
